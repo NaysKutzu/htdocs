e8efdce25692ef477a3c1344d71b600bc0dddbc8
NaysKutzu <87282334+NaysKutzu@users.noreply.github.com>
2023-05-11 11:37:34 +0200

    Test

---
+++ core/require/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @var array $_CONFIG
+ */
+$_CONFIG['db_host'] = "localhost";
+$_CONFIG['db_port'] = "3306";
+$_CONFIG['db_username'] = "root";
+$_CONFIG['db_password'] = "";
+$_CONFIG['db_name'] = "mythicalpass";
+?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php 
+include('core/functions/sql.php');
+
+?>
